7d78ff87104d05fe64e008f4f70b763889906f80
samuelPuerto66 <samlar4357@gmail.com>
2026-06-16 08:36:21 -0500

    asignar puerto dinámico en Render

--- app.py
+++ app.py
@@ -44,6 +44,5 @@ def inventario():
 
 # --- MODIFICACIÓN PARA LA NUBE (RENDER) ---
 if __name__ == "__main__":
-
     port = int(os.environ.get("PORT", 8000))
     app.run(host='0.0.0.0', port=port)
